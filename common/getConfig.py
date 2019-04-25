31b04109ae77f83790741f3e9ed1d32af7b7ea7f
An <2284223677@qq.com>
2019-04-25 22:41:15 +0800

    Initial commit

---
+++ common/getConfig.py
@@ -0,0 +1,20 @@
+import yaml
+import os
+
+
+def get_config(key):
+    """
+    解析yaml读取用户配置信息
+    :param key: 信息key
+    :return: data
+    """
+    f = open(os.path.join(os.path.dirname(os.path.dirname(__file__))) + '/config.yaml', encoding='utf8')
+    data = yaml.load(f)[key]
+    f.close()  # 记得关闭文件对象
+    return data
+
+
+if __name__ == '__main__':
+    for passenger in get_config('info')['passengers']:
+        print(passenger['name'])
+        print(passenger['ticket_type'])
